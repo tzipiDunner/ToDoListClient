4f626b1da29a9902f535daa2bb2dc3500cf3b604
TzipiDunner <tzipi0548529@gmail.com>
2023-06-25 01:06:04 +0300

    first commit

---
+++ src/service.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:5267";
+axios.interceptors.request.use(function(response)
+{
+  return response;
+},function(error){
+  return Promise.reject(error);
+});
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/todoitems`)
+    console.log(result);
+    return result.data;
+  },
+  addTask: async (name) => {
+  
+    const result = await axios.post(`/todoitems`, { id:1,name:name,isComplete:false});
+    console.log('addTask', name)
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    console.log('setCompleted', { id, isComplete })
+    await axios.put(`/todoitems/${id}`,{isComplete:{isComplete}})
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask')
+    const result = await axios.delete(`/todoitems/${id}`)
+    return result.data;
+  }
+};
